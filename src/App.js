e1abd0cd3bec1c98bdda4bdc70e9b4777b788916
Nuzair Ibrahim <nuzairb@gmail.com>
2019-01-16 21:21:43 +0500

    init

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import {recipes} from './tempList';
 import RecipeList from './components/RecipeList';
-import RecipeDetails from "./components/RecipeDetails";
+import RecipeDetails from './components/RecipeDetails';
 
 class App extends Component {
+	state = {
+		recipes: [],
+		url: "https://www.food2fork.com/api/search?key=720a0ca29a68252aa998c8a8f3e9010f"
+	};
+
+	async getRecipes() {
+		try{
+			const data = await fetch(this.state.url);
+			const jsonData = await data.json();
+			this.setState({
+				recipes: jsonData.recipes
+			});
+		}catch(error){
+			console.log(error);
+		}
+	}
+
 	render() {
 		return (
 			<React.Fragment>
--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import {recipe} from '../tempDetails';
 export default class RecipeDetails extends Component {
     render() {
         return (
             <React.Fragment>
                 <h1>Hello From RecipeDetails</h1>
             </React.Fragment>
-        )
+        );
     }
 }
